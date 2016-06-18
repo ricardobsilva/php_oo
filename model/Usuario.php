dcde00198b19eb089cf30ce54ff3f4d035ddf712
Lógica <Lgica>
2016-06-18 12:55:03 -0300

    criaçao de classes model

---
+++ model/Usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Usuario {
+    public $email;
+    public $nome;
+    public $nascimento;
+
+    
+    public function autenticar() {
+        echo 'usuario autenticou no sistema';
+    }
+    
+    public function sair() {
+        echo "usuario saiu do sistema";
+    }
+    
+}
